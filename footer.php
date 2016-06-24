aed0696da01e08ec49961c3db6bb8aa80e12f14a
blockbot <blockbot@gmail.com>
2016-06-24 10:21:59 -0400

    updated deferred styles, added GA

--- footer.php
+++ footer.php
@@ -3,16 +3,40 @@
             <p>Copyright &copy; <?php echo date(Y); ?> <a href="http://joeydehnert.com" target="_blank">Joey Dehnert</a></p>
 		</footer>
  -->
+
+        <noscript id="deferred-styles">
+            <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700"/>
+            <link rel="stylesheet" type="text/css" href="<?php bloginfo("template_url"); ?>/library/bower_components/bootstrap/dist/css/bootstrap.min.css"/>
+            <link rel="stylesheet" type="text/css" href="<?php bloginfo("template_url"); ?>/library/css/main.css"/>
+        </noscript>
+        
+        <script>
+        
+            var loadDeferredStyles = function() {
+                var addStylesNode = document.getElementById("deferred-styles");
+                var replacement = document.createElement("div");
+                replacement.innerHTML = addStylesNode.textContent;
+                document.body.appendChild(replacement)
+                addStylesNode.parentElement.removeChild(addStylesNode);
+            };
+
+            var raf = requestAnimationFrame || mozRequestAnimationFrame ||
+                webkitRequestAnimationFrame || msRequestAnimationFrame;
+            if (raf) raf(function() { window.setTimeout(loadDeferredStyles, 0); });
+            else window.addEventListener('load', loadDeferredStyles);
+        
+        </script>
+
 		<?php wp_footer(); ?>
 
-		<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
-            // (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            // function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            // e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            // e.src='//www.google-analytics.com/analytics.js';
-            // r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            // ga('create','********');ga('send','pageview');
+		<script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-1881290-34', 'auto');
+            ga('send', 'pageview');
         </script>
 
 	</body>
